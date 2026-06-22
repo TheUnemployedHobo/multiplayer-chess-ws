f74fed4d062aed2b8f46e001790b7929936569c9
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-06-22 17:31:34 +0330

    Refactor matchInsert to calculate and update user stats after match creation

--- src/controllers/matches-controller.ts
+++ src/controllers/matches-controller.ts
@@ -2,6 +2,8 @@ import type { RequestHandler } from "express"
 
 import db from "prisma/db"
 
+import { calculateNewElo } from "@/lib/utils"
+
 export const matchGet: RequestHandler = async (req, res) => {
   const { userId } = req.body
   const { skip, take } = req.query
@@ -20,10 +22,26 @@ export const matchGet: RequestHandler = async (req, res) => {
   res.json(foundMatches)
 }
 
+//? This is not practical. Should be moved to a independent function.
 export const matchInsert: RequestHandler = async (req, res) => {
   const { opponentId, result, userId } = req.body
 
-  await db.match.createMany({ data: { opponentId, result, userId } })
+  const match = await db.match.create({
+    data: { opponentId, result, userId },
+    select: {
+      opponent: { select: { stats: true } },
+      user: { select: { stats: true } },
+    },
+  })
+
+  const newStats = {
+    elo: calculateNewElo(match.user.stats.elo, match.opponent.stats.elo, result),
+    games: match.user.stats.games + 1,
+    losses: match.user.stats.losses + Number(result === "loss"),
+    wins: match.user.stats.wins + Number(result === "win"),
+  }
+
+  await db.user.update({ data: { stats: newStats }, where: { id: userId } })
 
   res.sendStatus(201)
 }
