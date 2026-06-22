800cf78556b3f76b1528479d9f89a5025d0a18a7
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-06-22 17:32:00 +0330

    Add calculateNewElo function to compute updated Elo ratings based on match results

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,3 +7,12 @@ export const jwtHelper = {
   sign: (userId: string) => jwt.sign({ userId }, JWT_SECRET, { expiresIn: "24h" }),
   verify: (token: string) => jwt.verify(token, JWT_SECRET) as { exp: number; iat: number; userId: number },
 }
+
+const resultInNum = { draw: 0.5, loss: 0, win: 1 } as const
+
+export const calculateNewElo = (userElo: number, opponentElo: number, result: "draw" | "loss" | "win", k = 32) => {
+  const expectedScore = 1 / (1 + Math.pow(10, (opponentElo - userElo) / 400))
+  const newElo = userElo + k * (resultInNum[result] - expectedScore)
+
+  return Math.round(newElo)
+}
