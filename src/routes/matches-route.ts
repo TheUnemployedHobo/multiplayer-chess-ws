2bed6e26882a91d0886cc9fd0d41084ba6511995
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-06-30 18:20:39 +0330

    Remove matchInsert route and related validation from matches-route

--- src/routes/matches-route.ts
+++ src/routes/matches-route.ts
@@ -1,13 +1,11 @@
 import { Router } from "express"
 import { asyncHandler } from "express-error-toolkit"
 
-import { matchGet, matchInsert } from "@/controllers/matches-controller"
-import { matchInsertVals } from "@/lib/validators"
-import { validateJwt, validateReq } from "@/middlewares/custom"
+import { matchGet } from "@/controllers/matches-controller"
+import { validateJwt } from "@/middlewares/custom"
 
 const route = Router()
 
 route.get("/", validateJwt, asyncHandler(matchGet))
-route.post("/", validateJwt, validateReq(matchInsertVals), asyncHandler(matchInsert))
 
 export default route
