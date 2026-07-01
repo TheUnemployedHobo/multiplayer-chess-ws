00bdb697a26c096e384d01ca5b8c35013f56cba5
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-07-01 15:05:57 +0330

    Update JWT expiration time from 24h to 30d in jwtHelper.sign

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,7 +4,7 @@ import jwt from "jsonwebtoken"
 const JWT_SECRET = process.env["JWT_SECRET"]!
 
 export const jwtHelper = {
-  sign: (userId: string) => jwt.sign({ userId }, JWT_SECRET, { expiresIn: "24h" }),
+  sign: (userId: string) => jwt.sign({ userId }, JWT_SECRET, { expiresIn: "30d" }),
   verify: (token: string) => jwt.verify(token, JWT_SECRET) as { exp: number; iat: number; userId: number },
 }
 
