64d8cda8385f3cbf4a825b3c4030ad168ca45706
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-07-03 17:35:06 +0330

    Fix order of asyncHandler for validateJwt middleware in users route

--- src/routes/users-route.ts
+++ src/routes/users-route.ts
@@ -9,8 +9,8 @@ const route = Router()
 
 route.post("/login", validateReq(userLoginVals), asyncHandler(userLogIn))
 route.post("/register", validateReq(userSignupVals), asyncHandler(userRegister))
-route.put("/", validateJwt, validateReq(userUpdateVals), asyncHandler(userUpdate))
-route.delete("/", validateJwt, asyncHandler(userDelete))
-route.get("/", validateJwt, asyncHandler(userGet))
+route.put("/", asyncHandler(validateJwt), validateReq(userUpdateVals), asyncHandler(userUpdate))
+route.delete("/", asyncHandler(validateJwt), asyncHandler(userDelete))
+route.get("/", asyncHandler(validateJwt), asyncHandler(userGet))
 
 export default route
