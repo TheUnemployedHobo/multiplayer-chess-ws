614409eb8805c87b9eb8baae1bd49269c115f576
The Unemployed Hobo <mohammadmehdibeheshti@yahoo.com>
2026-07-07 20:23:17 +0330

    Initialize Socket.IO server and update import statements in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,17 @@ import "dotenv/config"
 import express from "express"
 import { globalErrorHandler, notFoundHandler } from "express-error-toolkit"
 import { createServer } from "node:http"
+import { Server } from "socket.io"
 
 import middlewares from "./middlewares"
 import friendsRoute from "./routes/friends-route"
 import matchesRoute from "./routes/matches-route"
 import usersRoute from "./routes/users-route"
+import initiateSocketIO from "./socket"
 
 const app = express()
 const server = createServer(app)
+const io = new Server(server, { cors: { credentials: true, origin: "*" } })
 
 app.use(middlewares)
 app.use("/users", usersRoute)
@@ -17,6 +20,8 @@ app.use("/matches", matchesRoute)
 app.use("/friends", friendsRoute)
 app.use(notFoundHandler)
 app.use(globalErrorHandler)
+
+initiateSocketIO(io)
 
 const PORT = process.env["PORT"]!
 const SERVER_URL = process.env["SERVER_URL"]!
