65bfa34f6cf3ad426addd6c251c755241a4324e9
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-09 11:34:35 +0330

    feat: update user routes to replace userGet with userGetMe and add userGetAll for fetching all users

--- src/routes/users-route.ts
+++ src/routes/users-route.ts
@@ -1,7 +1,7 @@
 import { Router } from "express"
 import { asyncHandler } from "express-error-toolkit"
 
-import { userDelete, userGet, userLogIn, userRegister, userUpdate } from "@/controllers/users-controller"
+import { userDelete, userGetAll, userGetMe, userLogIn, userRegister, userUpdate } from "@/controllers/users-controller"
 import { userLoginVals, userSignupVals, userUpdateVals } from "@/lib/validators"
 import { validateJwt, validateReq } from "@/middlewares/custom"
 
@@ -11,6 +11,7 @@ route.post("/login", validateReq(userLoginVals), asyncHandler(userLogIn))
 route.post("/register", validateReq(userSignupVals), asyncHandler(userRegister))
 route.put("/", asyncHandler(validateJwt), validateReq(userUpdateVals), asyncHandler(userUpdate))
 route.delete("/", asyncHandler(validateJwt), asyncHandler(userDelete))
-route.get("/", asyncHandler(validateJwt), asyncHandler(userGet))
+route.get("/me", asyncHandler(validateJwt), asyncHandler(userGetMe))
+route.get("/all", asyncHandler(validateJwt), asyncHandler(userGetAll))
 
 export default route
