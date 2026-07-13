7e95d129d4ec2bf938065445338bb479eabc1f16
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-13 16:56:21 +0330

    feat: add online user management and friend status update functionality

---
+++ src/socket/utilities.ts
@@ -0,0 +1,23 @@
+import type { Server } from "socket.io"
+
+import db from "prisma/db"
+
+export const onlineUsers = new Map<string, { socketId: string; status: "online" | "playing" }>()
+
+export const friendStatusUpdate = async (io: Server, userId: string, status: "online" | "playing" | undefined) => {
+  try {
+    const friends = await db.friend.findMany({
+      select: { friendId: true },
+      where: { userId },
+    })
+
+    friends.forEach(({ friendId }) => {
+      const onlineFriend = onlineUsers.get(friendId)
+      if (onlineFriend) io.to(onlineFriend.socketId).emit("friends:status", { status, userId })
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const sendOnlineCount = (io: Server) => io.emit("users:online-count", onlineUsers.size)
