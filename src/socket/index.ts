9797135952fa6a52a3f2097c4c179825ee033011
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-15 14:38:53 +0330

    refactor: rename friendsSocket to registerFriendEvents and update related function calls

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,8 +2,8 @@ import type { Server } from "socket.io"
 
 import { jwtHelper } from "@/lib/utils"
 
-import friendsSocket from "./friends-socket"
-import { friendStatusUpdate, onlineUsers, sendOnlineCount } from "./utilities"
+import registerFriendEvents from "./friends-socket"
+import { onlineUsers, sendOnlineCount, updateFriendStatus } from "./utilities"
 
 const initiateSocketIO = (io: Server) => {
   io.use((socket, next) => {
@@ -26,12 +26,12 @@ const initiateSocketIO = (io: Server) => {
     onlineUsers.set(userId, { socketId: socket.id, status: "online" })
 
     sendOnlineCount(io)
-    friendsSocket(io, socket)
+    registerFriendEvents(io, socket)
 
     socket.on("disconnect", () => {
       onlineUsers.delete(userId)
       sendOnlineCount(io)
-      friendStatusUpdate(io, userId, undefined)
+      updateFriendStatus(io, userId, undefined)
     })
   })
 }
