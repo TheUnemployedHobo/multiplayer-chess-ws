f55a31533cfed197a49608beb3b4de148eb518af
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-15 15:01:39 +0330

    feat: implement bot event handling for chess game

---
+++ src/socket/bot-events.ts
@@ -0,0 +1,23 @@
+import type { Socket } from "socket.io"
+
+import { Chess } from "chess.js"
+
+import { createEngine } from "@/lib/stockfish"
+
+import { botGames } from "./utilities"
+
+const registerBotEvents = (socket: Socket) => {
+  socket.on("bot:start", async (difficulty) => {
+    const chess = new Chess()
+    const engine = await createEngine()
+
+    engine.sendCommand(`setoption name Skill Level value ${difficulty}`)
+    engine.sendCommand("ucinewgame")
+
+    botGames.set(socket.id, { chess, engine })
+  })
+  socket.on("bot:move", () => {})
+  socket.on("bot:resign", () => {})
+}
+
+export default registerBotEvents
