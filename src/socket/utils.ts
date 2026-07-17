fc90484ac70f0869e0cba4bd5a16d2520ead2748
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-17 19:35:40 +0330

    refactor: update botGames structure to include move history and use AiLevelsType

--- src/socket/utils.ts
+++ src/socket/utils.ts
@@ -3,8 +3,11 @@ import type { Server } from "socket.io"
 import { Game } from "js-chess-engine"
 import db from "prisma/db"
 
+export type AiLevelsType = 1 | 2 | 3 | 4 | 5
+export type MovePayloadType = { from: string; to: string }
+
 export const onlineUsers = new Map<string, { socketId: string; status: "online" | "playing" }>()
-export const botGames = new Map<string, { game: Game; level: 1 | 2 | 3 | 4 | 5 }>()
+export const botGames = new Map<string, { game: Game; history: MovePayloadType[]; level: AiLevelsType }>()
 
 export const updateFriendStatus = async (io: Server, userId: string, status: "online" | "playing" | undefined) => {
   try {
