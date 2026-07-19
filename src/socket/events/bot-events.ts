ffdda4fb96f919e6dfe2deea1c6dd165b2b8b9a2
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-19 22:15:34 +0330

    feat: refactor bot event handling to integrate Chess instance and improve game logic

--- src/socket/events/bot-events.ts
+++ src/socket/events/bot-events.ts
@@ -1,75 +1,74 @@
 import type { Server, Socket } from "socket.io"
 
+import { Chess, type PieceSymbol } from "chess.js"
 import { Game } from "js-chess-engine"
 
-import { type AiLevelsType, botGames, type MovePayloadType, updateFriendStatus } from "../utils"
+import { type AiLevelsType, botGames, determineGameResult, updateFriendStatus } from "../utils"
 
 const registerBotEvents = (io: Server, socket: Socket) => {
   const { userId } = socket.data
 
   socket.on("bot:start", (skill: AiLevelsType) => {
-    botGames.delete(socket.id)
-    botGames.set(socket.id, { game: new Game(), history: [], level: skill })
-
+    botGames.set(userId, { chess: new Chess(), engine: new Game(), level: skill })
     updateFriendStatus(io, userId, "playing")
-
     socket.emit("bot:start", undefined)
   })
 
-  socket.on("bot:move", ({ from, to }: MovePayloadType) => {
-    const instance = botGames.get(socket.id)
+  socket.on("bot:move", ({ from, promotion, to }: { from: string; promotion: string; to: string }) => {
+    const instance = botGames.get(userId)
     if (!instance) return
 
-    try {
-      instance.game.move(from.toUpperCase(), to.toUpperCase())
-      instance.history.push({ from: from.toUpperCase(), to: to.toUpperCase() })
-    } catch {
-      return
-    }
+    if (!instance.chess.move({ from, promotion, to })) return
 
-    const playerBoard = instance.game.exportJson()
-    if (playerBoard.isFinished) {
-      socket.emit("bot:finished", undefined)
-      botGames.delete(socket.id)
+    instance.engine.move(from.toUpperCase(), to.toUpperCase())
+
+    if (promotion && promotion !== "q")
+      instance.engine.setPiece(to.toUpperCase(), promotion.toUpperCase() as PieceSymbol)
+
+    if (instance.chess.isGameOver()) {
+      botGames.delete(userId)
       updateFriendStatus(io, userId, "online")
+      socket.emit("bot:finished", determineGameResult({ chess: instance.chess }))
       return
     }
 
-    const { board, move } = instance.game.ai({ level: instance.level, randomness: 0 })
+    const { move } = instance.engine.ai({ level: instance.level, randomness: 0 })
 
     const [entry] = Object.entries(move)
-    if (!entry) return
 
-    const [botFrom, botTo] = entry
+    const [botFrom, botTo] = entry!
 
-    instance.history.push({ from: botFrom, to: botTo })
+    instance.chess.move({ from: botFrom.toLowerCase(), promotion: "q", to: botTo.toLowerCase() })
+
     socket.emit("bot:move", { from: botFrom.toLowerCase(), to: botTo.toLowerCase() })
 
-    if (board.isFinished) {
-      socket.emit("bot:finished", undefined)
-      botGames.delete(socket.id)
+    if (instance.chess.isGameOver()) {
+      botGames.delete(userId)
       updateFriendStatus(io, userId, "online")
+      socket.emit("bot:finished", determineGameResult({ chess: instance.chess }))
     }
   })
 
   socket.on("bot:resign", () => {
-    botGames.delete(socket.id)
-    updateFriendStatus(io, userId, "online")
     socket.emit("bot:resign", undefined)
+    updateFriendStatus(io, userId, "online")
+    botGames.delete(userId)
   })
 
   socket.on("bot:undo", () => {
-    const instance = botGames.get(socket.id)
+    const instance = botGames.get(userId)
     if (!instance) return
 
-    instance.history.pop()
-    instance.history.pop()
+    instance.chess.undo()
+    instance.chess.undo()
 
-    const game = new Game()
+    const engine = new Game()
 
-    instance.history.forEach(({ from, to }) => game.move(from.toUpperCase(), to.toUpperCase()))
+    instance.chess
+      .history({ verbose: true })
+      .forEach(({ from, to }) => engine.move(from.toUpperCase(), to.toUpperCase()))
 
-    instance.game = game
+    instance.engine = engine
 
     socket.emit("bot:undo", undefined)
   })
