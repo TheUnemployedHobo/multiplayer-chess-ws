555d820856a24b53ccb2b6a80a3ed32444f80899
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-21 21:58:55 +0330

    feat: enhance game event handling by adding draw offer functionality and improving event responses

--- src/socket/events/game-events.ts
+++ src/socket/events/game-events.ts
@@ -1,7 +1,108 @@
 import type { Server, Socket } from "socket.io"
 
+import { activeGames, determineGameResult, type MovePayloadType, playerRooms, updateFriendStatus } from "../utils"
+
 const registerGameEvents = (io: Server, socket: Socket) => {
-  // ...
+  const { userId } = socket.data
+
+  socket.on("game:move", ({ from, promotion, to }: MovePayloadType) => {
+    const roomId = playerRooms.get(userId)
+    if (!roomId) return
+
+    const game = activeGames.get(roomId)
+    if (!game) return
+
+    const color = game.chess.turn()
+
+    if (color === "w" && game.white.userId !== userId) return
+    if (color === "b" && game.black.userId !== userId) return
+
+    const move = game.chess.move({ from, promotion, to })
+    if (!move) return
+
+    socket.to(roomId).emit("game:move", { from: move.from, promotion: move.promotion, to: move.to })
+
+    const result = determineGameResult(game.chess)
+    if (!result) return
+
+    activeGames.delete(roomId)
+    playerRooms.delete(game.white.userId)
+    playerRooms.delete(game.black.userId)
+
+    updateFriendStatus(io, game.white.userId, "online")
+    updateFriendStatus(io, game.black.userId, "online")
+
+    io.to(roomId).emit("game:finished", result)
+  })
+
+  socket.on("game:resign", () => {
+    const roomId = playerRooms.get(userId)
+    if (!roomId) return
+
+    const game = activeGames.get(roomId)
+    if (!game) return
+
+    const resignedColor = game.white.userId === userId ? "White" : "Black"
+    const winner = resignedColor === "White" ? "Black" : "White"
+
+    activeGames.delete(game.roomId)
+    playerRooms.delete(game.white.userId)
+    playerRooms.delete(game.black.userId)
+
+    updateFriendStatus(io, game.white.userId, "online")
+    updateFriendStatus(io, game.black.userId, "online")
+
+    io.to(game.roomId).emit("game:resign", { result: `${resignedColor} resigned`, winner })
+  })
+
+  socket.on("game:draw-offer", () => {
+    const roomId = playerRooms.get(userId)
+    if (!roomId) return
+
+    const game = activeGames.get(roomId)
+    if (!game) return
+
+    const offerorColor = game.white.userId === userId ? "white" : "black"
+    const offereeColor = offerorColor === "white" ? "black" : "white"
+
+    socket.emit("game:draw-offer", { message: `Draw offer sent to ${offereeColor}`, offerRole: "offeror" })
+    socket.to(game[offereeColor].socketId).emit("game:draw-offer", { message: `${offerorColor} offered a draw`, offerRole: "offeree" })
+  })
+
+  socket.on("game:draw-offer:decline", () => {
+    const roomId = playerRooms.get(userId)
+    if (!roomId) return
+
+    const game = activeGames.get(roomId)
+    if (!game) return
+
+    const declinerColor = game.white.userId === userId ? "white" : "black"
+    const offerorColor = declinerColor === "white" ? "black" : "white"
+
+    socket.emit("game:draw-offer:decline", "You declined the draw offer")
+    socket.to(game[offerorColor].socketId).emit("game:draw-offer:decline", `${declinerColor} declined your draw offer`)
+  })
+
+  socket.on("game:draw-offer:accept", () => {
+    const roomId = playerRooms.get(userId)
+    if (!roomId) return
+
+    const game = activeGames.get(roomId)
+    if (!game) return
+
+    const acceptorColor = game.white.userId === userId ? "white" : "black"
+    const offerorColor = acceptorColor === "white" ? "black" : "white"
+
+    activeGames.delete(roomId)
+    playerRooms.delete(game.white.userId)
+    playerRooms.delete(game.black.userId)
+
+    updateFriendStatus(io, game.white.userId, "online")
+    updateFriendStatus(io, game.black.userId, "online")
+
+    socket.emit("game:draw-offer:accept", undefined)
+    socket.to(game[offerorColor].socketId).emit("game:draw-offer:accept", undefined)
+  })
 }
 
 export default registerGameEvents
