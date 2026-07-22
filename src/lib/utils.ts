915986f9b243b8658003fb92bf1ff07aec769a64
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-22 18:38:16 +0330

    feat: enhance utils by adding game result determination and friend status updates

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,11 @@
 import "dotenv/config"
+import type { Chess } from "chess.js"
+import type { Server } from "socket.io"
+
 import jwt from "jsonwebtoken"
+import db from "prisma/db"
+
+import { onlineUsers } from "./storage"
 
 const JWT_SECRET = process.env["JWT_SECRET"]!
 
@@ -8,11 +14,34 @@ export const jwtHelper = {
   verify: (token: string) => jwt.verify(token, JWT_SECRET) as { exp: number; iat: number; userId: number },
 }
 
-const resultInNum = { draw: 0.5, loss: 0, win: 1 } as const
-
 export const calculateNewElo = (userElo: number, opponentElo: number, result: "draw" | "loss" | "win", k = 32) => {
   const expectedScore = 1 / (1 + Math.pow(10, (opponentElo - userElo) / 400))
-  const newElo = userElo + k * (resultInNum[result] - expectedScore)
+  const newElo = userElo + k * ({ draw: 0.5, loss: 0, win: 1 }[result] - expectedScore)
 
   return Math.round(newElo)
+}
+
+export const updateFriendStatus = async (io: Server, userId: string, status: "online" | "playing" | undefined) => {
+  try {
+    const friends = await db.friend.findMany({ select: { friendId: true }, where: { userId } })
+
+    friends.forEach(({ friendId }) => {
+      const onlineFriend = onlineUsers.get(friendId)
+      if (onlineFriend) io.to(onlineFriend.socketId).emit("friend:status", { status, userId })
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export const sendOnlineCount = (io: Server) => io.emit("users:online-count", onlineUsers.size)
+
+export const determineGameResult = (chess: Chess) => {
+  if (chess.isCheckmate()) return { result: "Checkmate", winner: chess.turn() === "w" ? "Black" : "White" }
+  if (chess.isStalemate()) return { result: "Stalemate", winner: null }
+  if (chess.isInsufficientMaterial()) return { result: "Insufficient material", winner: null }
+  if (chess.isThreefoldRepetition()) return { result: "Three fold repetition", winner: null }
+  if (chess.isDrawByFiftyMoves()) return { result: "50 move rule", winner: null }
+
+  return null
 }
