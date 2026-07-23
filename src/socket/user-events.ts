9e3ae2339aa4d35321e5961212f9d8bb6de042e3
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-23 13:36:41 +0330

    feat: export registerUserEvents function for improved modularity

--- src/socket/user-events.ts
+++ src/socket/user-events.ts
@@ -2,10 +2,8 @@ import type { Server, Socket } from "socket.io"
 
 import { sendOnlineCount } from "@/lib/utils"
 
-const registerUserEvents = (io: Server, socket: Socket) => {
+export default function registerUserEvents(io: Server, socket: Socket) {
   socket.on("users:online-count", () => {
     sendOnlineCount(io)
   })
 }
-
-export default registerUserEvents
