59791c9f079d5c8834f4f7c19234efc838615855
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-23 16:47:03 +0330

    refactor: remove commented-out matchInsert function for cleaner code

--- src/controllers/matches-controller.ts
+++ src/controllers/matches-controller.ts
@@ -2,8 +2,6 @@ import type { RequestHandler } from "express"
 
 import db from "prisma/db"
 
-// import { calculateNewElo } from "@/lib/utils"
-
 export const matchGet: RequestHandler = async (req, res) => {
   const { userId } = req.body
   const { skip, take } = req.query
@@ -21,28 +19,3 @@ export const matchGet: RequestHandler = async (req, res) => {
 
   res.json(foundMatches)
 }
-
-//? This is not practical. It should be moved to an independent function.
-
-// export const matchInsert: RequestHandler = async (req, res) => {
-//   const { opponentId, result, userId } = req.body
-
-//   const match = await db.match.create({
-//     data: { opponentId, result, userId },
-//     select: {
-//       opponent: { select: { stats: true } },
-//       user: { select: { stats: true } },
-//     },
-//   })
-
-//   const newStats = {
-//     elo: calculateNewElo(match.user.stats.elo, match.opponent.stats.elo, result),
-//     games: match.user.stats.games + 1,
-//     losses: match.user.stats.losses + Number(result === "loss"),
-//     wins: match.user.stats.wins + Number(result === "win"),
-//   }
-
-//   await db.user.update({ data: { stats: newStats }, where: { id: userId } })
-
-//   res.sendStatus(201)
-// }
