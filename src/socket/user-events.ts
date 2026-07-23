6ef5203706b55c6783da300768c489fa65064f89
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-23 13:57:28 +0330

    feat: move sendOnlineCount function to user-events.ts for better organization

--- src/socket/user-events.ts
+++ src/socket/user-events.ts
@@ -1,6 +1,8 @@
 import type { Server, Socket } from "socket.io"
 
-import { sendOnlineCount } from "@/lib/utils"
+import { onlineUsers } from "@/lib/storage"
+
+export const sendOnlineCount = (io: Server) => io.emit("users:online-count", onlineUsers.size)
 
 export default function registerUserEvents(io: Server, socket: Socket) {
   socket.on("users:online-count", () => {
