f9c6ed9bd2330876cedcba523285ec172a26a799
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-23 13:34:43 +0330

    feat: implement createGame function to handle game creation and matchmaking

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,11 +1,11 @@
 import "dotenv/config"
-import type { Chess } from "chess.js"
 import type { Server } from "socket.io"
 
+import { Chess } from "chess.js"
 import jwt from "jsonwebtoken"
 import db from "prisma/db"
 
-import { onlineUsers } from "./storage"
+import { activeGames, onlineUsers, playerRooms } from "./storage"
 
 const JWT_SECRET = process.env["JWT_SECRET"]!
 
@@ -14,14 +14,14 @@ export const jwtHelper = {
   verify: (token: string) => jwt.verify(token, JWT_SECRET) as { exp: number; iat: number; userId: number },
 }
 
-export const calculateNewElo = (userElo: number, opponentElo: number, result: "draw" | "loss" | "win", k = 32) => {
+export function calculateNewElo(userElo: number, opponentElo: number, result: "draw" | "loss" | "win", k = 32) {
   const expectedScore = 1 / (1 + Math.pow(10, (opponentElo - userElo) / 400))
   const newElo = userElo + k * ({ draw: 0.5, loss: 0, win: 1 }[result] - expectedScore)
 
   return Math.round(newElo)
 }
 
-export const updateFriendStatus = async (io: Server, userId: string, status: "online" | "playing" | undefined) => {
+export async function updateFriendStatus(io: Server, userId: string, status: "online" | "playing" | undefined) {
   try {
     const friends = await db.friend.findMany({ select: { friendId: true }, where: { userId } })
 
@@ -36,7 +36,60 @@ export const updateFriendStatus = async (io: Server, userId: string, status: "on
 
 export const sendOnlineCount = (io: Server) => io.emit("users:online-count", onlineUsers.size)
 
-export const determineGameResult = (chess: Chess) => {
+export async function createGame({ blackId, io, whiteId }: { blackId: string; io: Server; whiteId: string }) {
+  try {
+    const [whiteUser, blackUser] = await db.$transaction([
+      db.user.findUnique({
+        select: { avatar: true, id: true, stats: { select: { elo: true } }, username: true },
+        where: { id: whiteId },
+      }),
+      db.user.findUnique({
+        select: { avatar: true, id: true, stats: { select: { elo: true } }, username: true },
+        where: { id: blackId },
+      }),
+    ])
+    if (!whiteUser || !blackUser) return
+
+    const whiteSocket = onlineUsers.get(whiteUser.id)
+    const blackSocket = onlineUsers.get(blackUser.id)
+    if (!whiteSocket || !blackSocket) return
+
+    const roomId = crypto.randomUUID()
+
+    io.sockets.sockets.get(whiteSocket.socketId)?.join(roomId)
+    io.sockets.sockets.get(blackSocket.socketId)?.join(roomId)
+
+    playerRooms.set(whiteUser.id, roomId)
+    playerRooms.set(blackUser.id, roomId)
+
+    activeGames.set(roomId, {
+      black: { socketId: blackSocket.socketId, userId: blackUser.id },
+      chess: new Chess(),
+      white: { socketId: whiteSocket.socketId, userId: whiteUser.id },
+    })
+
+    updateFriendStatus(io, whiteUser.id, "playing")
+    updateFriendStatus(io, blackUser.id, "playing")
+
+    io.to(whiteSocket.socketId).emit("matchmaking:join", {
+      avatar: blackUser.avatar,
+      color: "white",
+      elo: blackUser.stats.elo,
+      username: blackUser.username,
+    })
+
+    io.to(blackSocket.socketId).emit("matchmaking:join", {
+      avatar: whiteUser.avatar,
+      color: "black",
+      elo: whiteUser.stats.elo,
+      username: whiteUser.username,
+    })
+  } catch (err) {
+    console.error(err)
+  }
+}
+
+export function determineGameResult(chess: Chess) {
   if (chess.isCheckmate()) return { result: "Checkmate", winner: chess.turn() === "w" ? "Black" : "White" }
   if (chess.isStalemate()) return { result: "Stalemate", winner: null }
   if (chess.isInsufficientMaterial()) return { result: "Insufficient material", winner: null }
