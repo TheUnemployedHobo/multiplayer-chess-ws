3e1e616c42bda5da5457baf39c652810ab1d5256
TheUnemplyedHobo <freakypumpkineer@gmail.com>
2026-07-23 14:34:54 +0330

    refactor: simplify online count event handler for improved readability

--- src/socket/user-events.ts
+++ src/socket/user-events.ts
@@ -5,7 +5,5 @@ import { onlineUsers } from "@/lib/storage"
 export const sendOnlineCount = (io: Server) => io.emit("users:online-count", onlineUsers.size)
 
 export default function registerUserEvents(io: Server, socket: Socket) {
-  socket.on("users:online-count", () => {
-    sendOnlineCount(io)
-  })
+  socket.on("users:online-count", () => sendOnlineCount(io))
 }
